069254646d8bb0bc1387d0647adde3d550e8015d
Diego Lopes <diegolopes0501@gmail.com>
2024-12-07 18:51:00 -0300

    Add test service to Docker and initialize user factory
    Included a new test service to the Docker Compose setup to streamline testing. Modified the user factory to include essential attributes for user creation. Updated seed files to ensure the initial admin user creation when no users exist. Integrated 'devise-bootstrap-views' gem to enhance authentication views styling.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,10 @@
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
+FactoryBot.create(:user, email: 'admin@example.com') if User.count.zero?
+
 if Proponent.count.zero?
-  50.times do
+  10.times do
     FactoryBot.create(:proponent)
   end
 end
