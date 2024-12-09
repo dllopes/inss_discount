6d4a418c54b6f6f42376b323d31bf39e79a6fd2b
Diego Lopes <diegolopes0501@gmail.com>
2024-12-09 14:40:56 -0300

    Format CPF uniformly across views.
    Implemented a helper method `format_cpf` in `ApplicationHelper` to ensure CPF numbers are consistently formatted as 'XXX.XXX.XXX-XX'. Updated relevant views to use this new method, enhancing readability and data presentation.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def format_cpf(cpf)
+    return '' if cpf.blank?
+
+    cpf.to_s.gsub(/\D/, '').rjust(11, '0').gsub(/(\d{3})(\d{3})(\d{3})(\d{2})/, '\1.\2.\3-\4')
+  end
 end
